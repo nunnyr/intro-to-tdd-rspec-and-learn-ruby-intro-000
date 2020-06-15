711aa968388c4c9227e13240a984b0bc30b61e0c
Nunny Reyes <nunnyreyes@gmail.com>
2020-06-15 19:43:15 +0000

    Automatically backed up by Learn

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,6 @@
+def current_age_for_birth_year(birth_year)
+end
+
+#no method error refers to no method being defined
+#an argument method means that a method got called with an argument but the one that 
+#we did define does not accept as argument.
--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../current_age_for_birth_year.rb'
 
+#this first line of code breaks down the way the file is loaded
+#then we say that it needs to take into account proper Ruby
+
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
@@ -7,3 +10,6 @@ describe "current_age_for_birth_year method" do
     expect(age_of_person).to eq(19)
   end
 end
+
+#describe literally describes what we are 
+#it is the expectation or behavior of that method
